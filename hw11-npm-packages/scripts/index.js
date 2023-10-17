e207094961f5c0b618152db7425a713ada19ce0a
LevkoBe <lbeniakh@kse.org.ua>
2023-10-17 15:14:36 +0300

    mooment

--- hw11-npm-packages/scripts/index.js
+++ hw11-npm-packages/scripts/index.js
@@ -1,10 +1,3 @@
-// import moment from "moment";
-// import { reverse } from "\\node_modules\\@types\\lodash\\index.d.ts";
-// import { reverse } from "./node_modules/lodash/index.d.ts";
-// import { reverse } from "lodash";
-// import $ from "\\node_modules\\@types\\jquery\\index.d.ts";
-// import $ from "jquery";
-
 let data = [1, 2, 3, 4, 5];
 
 // jQuery
@@ -17,10 +10,8 @@ displayData();
 $("#modify-data-button").on("click", function () {
   // Lodash
   data = _.reverse(data);
-  // data = reverse(data);
   displayData();
 });
 
-// Example usage
 let now = moment();
 console.log(now.format("MMMM Do YYYY, h:mm:ss a"));
