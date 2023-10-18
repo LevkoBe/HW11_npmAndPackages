248c28797f486148dee87e8da6911a0a43635b62
LevkoBe <lbeniakh@kse.org.ua>
2023-10-18 20:09:15 +0300

    Some functionality

--- hw11-npm-packages/scripts/index.js
+++ hw11-npm-packages/scripts/index.js
@@ -1,4 +1,5 @@
 let data = [1, 2, 3, 4, 5];
+let chartData = data.slice();
 
 function displayData() {
   $("#data-container").html(JSON.stringify(data));
@@ -8,6 +9,31 @@ displayData();
 
 $("#modify-data-button").on("click", function () {
   data = _.reverse(data);
+  chartData = data.slice();
+  displayData();
+});
+
+$("#generate-random-data").on("click", function () {
+  data = Array.from({ length: 5 }, () => Math.floor(Math.random() * 10));
+  chartData = data.slice();
+  displayData();
+});
+
+$("#chunk-data").on("click", function () {
+  data = _.chunk(data, 2);
+  chartData = data.slice();
+  displayData();
+});
+
+$("#sort-data").on("click", function () {
+  data = _.sortBy(data);
+  chartData = data.slice();
+  displayData();
+});
+
+$("#shuffle-data").on("click", function () {
+  data = _.shuffle(data);
+  chartData = data.slice();
   displayData();
 });
 
@@ -22,8 +48,23 @@ let myChart = new Chart(ctx, {
     datasets: [
       {
         label: "Sample Data",
-        data: data,
+        data: chartData,
+        backgroundColor: ["rgba(255, 99, 132, 0.2)", "rgba(54, 162, 235, 0.2)", "rgba(255, 206, 86, 0.2)", "rgba(75, 192, 192, 0.2)", "rgba(153, 102, 255, 0.2)"],
+        borderColor: ["rgba(255, 99, 132, 1)", "rgba(54, 162, 235, 1)", "rgba(255, 206, 86, 1)", "rgba(75, 192, 192, 1)", "rgba(153, 102, 255, 1)"],
+        borderWidth: 1,
       },
     ],
   },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  },
+});
+
+$("#add-to-graph").on("click", function () {
+  myChart.data.datasets[0].data = chartData;
+  myChart.update();
 });
